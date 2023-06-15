34a545bafcf44b4aed3444ff785512cf8c61a14b
motich351 <motih.iss1993@gmail.com>
2023-06-15 14:24:27 +0500

    Worker full ready!

--- endpoints/worker.py
+++ endpoints/worker.py
@@ -28,42 +28,42 @@ async def get_all():
     return WorkersOut(workers=workers_dto)
 
 
-# @router.post("/create_client", response_model=ClientOut)
-# async def create_client(client: ClientIn):
-#     session = get_session()
-#     orm_client = Client(**client.dict())
-#     session.add(orm_client)
-#     print(orm_client.__dict__)
-#     session.commit()
-#     print(orm_client.__dict__)
-#     client_dto = ClientOut(**orm_client.__dict__)
-#     return client_dto
-#
-#
-# @router.delete("/delete_client/{client_id}", response_model=ClientOut)
-# async def delete_client(client_id: int, session: Session = Depends(get_session)):
-#     client: Client = session.query(Client).get(client_id)
-#     if client:
-#         client_dto = ClientOut(**client.__dict__)
-#         session.delete(client)
-#         session.commit()
-#         return client_dto
-#     else:
-#         raise HTTPException(status_code=404,
-#                             detail=f"Client with id {client_id} not found!")
-#
-#
-# @router.post("/update_client", response_model=ClientOut)
-# async def update_client(client: ClientIn):
-#     session = get_session()
-#
-#     orm_client = session.query(Client).get(client.client_id)
-#     #session.add(orm_client)
-#     orm_client.client_name = client.client_name
-#     orm_client.client_city = client.client_city
-#
-#     print(orm_client.__dict__)
-#     session.commit()
-#     print(orm_client.__dict__)
-#     client_dto = ClientOut.from_orm(orm_client)
-#     return client_dto
+@router.post("/create_worker", response_model=WorkerOut)
+async def create_worker(worker: WorkerIn):
+    session = get_session()
+    orm_worker = Worker(**worker.dict())
+    session.add(orm_worker)
+    print(orm_worker.__dict__)
+    session.commit()
+    print(orm_worker.__dict__)
+    worker_dto = WorkerOut(**orm_worker.__dict__)
+    return worker_dto
+
+
+@router.delete("/delete_worker/{worker_id}", response_model=WorkerOut)
+async def delete_worker(worker_id: int, session: Session = Depends(get_session)):
+    worker: Worker = session.query(Worker).get(worker_id)
+    if worker:
+        worker_dto = WorkerOut(**worker.__dict__)
+        session.delete(worker)
+        session.commit()
+        return worker_dto
+    else:
+        raise HTTPException(status_code=404,
+                            detail=f"Worker with id {worker_id} not found!")
+
+
+@router.post("/update_workert", response_model=WorkerOut)
+async def update_worker(worker: WorkerIn):
+    session = get_session()
+
+    orm_worker = session.query(Worker).get(worker.worker_id)
+    #session.add(orm_worker)
+    orm_worker.fullname = worker.fullname
+    orm_worker.jobrank = worker.jobrank
+    orm_worker.salary = worker.salary
+
+    print(orm_worker.__dict__)
+    session.commit()
+    worker_dto = WorkerOut.from_orm(orm_worker)
+    return worker_dto
